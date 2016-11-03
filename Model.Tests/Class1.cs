db8643417f325a4b995adcf01ccec26ced58357a
Łukasz Kępka <ukasz Kpka@DESKTOP-UEGAF2P>
2016-11-03 17:50:03 +0100

    Uploaded project files

---
+++ Model.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ModelTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void test()
+        {
+            Assert.AreEqual(Model.Class1.fun(), 2);
+        }
+    }
+}
